17a9b17dd2232561bde28df1a837a16bc49b252e
leetaaan <119284828+leetaaan@users.noreply.github.com>
2024-10-17 21:07:28 +0700

    lt coffee client-updated next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          hostname: "firebasestorage.googleapis.com",
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
-  
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        hostname: "firebasestorage.googleapis.com",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
